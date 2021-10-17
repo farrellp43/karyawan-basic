6999d6489e1aa1a18aade427d8fa8576f408c1b9
farrellp43 <farrellp43@gmail.com>
2021-10-17 13:41:33 +0700

    Added seeder for users & employees

--- database/seeds/EmployeeSeeder.php
+++ database/seeds/EmployeeSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EmployeeSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class EmployeeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('employees')->insert([
+            [
+                'name' => 'Farrell Denando',
+                'email' => 'farrellp43@gmail.com',
+                'age' => 23,
+                'salary' => 3000000,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ], [
+                'name' => 'Kyoma',
+                'email' => 'kyoma69@gmail.com',
+                'age' => 69,
+                'salary' => 69000000,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ]);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'password' => bcrypt('password'),
+            'email' => 'admin@admin.com',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+        ]);
+    }
+}
